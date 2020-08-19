41b614e47ab528612d5635ff769d69245cd48e83
dyyyl <hey@dyyyl.rocks>
2020-08-19 19:44:25 +0200

    update import

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,12 @@
 import { ApolloServer } from 'apollo-server';
-import ApolloServerLambda from 'apollo-server-lambda';
+import { ApolloServer as ApolloServerLambda } from 'apollo-server-lambda';
 
 import GhibliAPI from './api/GhibliAPI';
 import resolvers from './graphql/resolvers';
 import typeDefs from './graphql/typeDefs';
 
-export const createLambdaServer = (): ApolloServerLambda.ApolloServer => {
-  return new ApolloServerLambda.ApolloServer({
+export const createLambdaServer = (): ApolloServerLambda => {
+  return new ApolloServerLambda({
     typeDefs,
     resolvers,
     dataSources: () => ({
